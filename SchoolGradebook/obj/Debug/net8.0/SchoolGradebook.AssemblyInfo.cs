25de764f6ba33f6d1997717e59ef2b62799d9f83
niribareva22 <niribareva22@codingburgas.bg>
2026-04-14 09:19:06 +0300

    Add index and create view for student

--- SchoolGradebook/obj/Debug/net8.0/SchoolGradebook.AssemblyInfo.cs
+++ SchoolGradebook/obj/Debug/net8.0/SchoolGradebook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolGradebook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad20e6430cf7d4285f02790ad3555f1c3bbb1c37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3c40bdd0580087a1702ec72e631e9dbcc772472")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolGradebook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolGradebook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
