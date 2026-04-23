17c2d2d77fca0306bf423fb6a537faffa0af0e35
niribareva22 <niribareva22@codingburgas.bg>
2026-04-23 23:06:16 +0300

    Implement GradesController and link it with students

---
+++ SchoolGradebook/Controllers/GradesController.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using SchoolGradebook.Data;
+using SchoolGradebook.Models;
+
+namespace SchoolGradebook.Controllers;
+
+public class GradesController : Controller
+{
+    private readonly ApplicationDbContext _context;
+
+    public GradesController(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<IActionResult> Index()
+    {
+        var grades = _context.Grades.Include(g => g.Student);
+        return View(await grades.ToListAsync());
+    }
+
+    public IActionResult Create()
+    {
+        ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Name");
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(Grade grade)
+    {
+        if (ModelState.IsValid)
+        {
+            grade.CreatedAt = DateTime.Now;
+            _context.Add(grade);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+        ViewData["StudentId"] = new SelectList(_context.Students, "Id", "Name", grade.StudentId);
+        return View(grade);
+    }
+}
